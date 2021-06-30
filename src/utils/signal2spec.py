43db83cc3779cac01b42f2d126dd2a75101c117d
Ithihas Madala <58927988+ithihasmadala@users.noreply.github.com>
2021-06-30 18:13:05 +0530

    Update signal2spec.py

--- src/utils/signal2spec.py
+++ src/utils/signal2spec.py
@@ -1,6 +1,10 @@
 import numpy as np
 from scipy import signal
+
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+
 
 
 class toSpectrogram():
@@ -22,11 +26,13 @@ class toSpectrogram():
         t, f, Sxx = self.generateSpec()
         fig = plt.figure(1)
         plt.pcolormesh(t, f, Sxx, shading = self.color)
+        plt.subplots_adjust(left=0,right=1,bottom=0,top=1)
+        plt.axis('tight')
+        plt.axis('off')
         fig.canvas.draw()
         data = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
         data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
         plt.close(fig)
-
         return data
 
     def inPNG(self, path):
